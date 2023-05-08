dfc045616a8d394284c966ff2f0d9c1847763f67
Shadman Rohan <shadmanrohan@gmail.com>
2023-05-09 00:33:02 +0600

    added docstring

--- text-evaluation-metrics/tem.py
+++ text-evaluation-metrics/tem.py
@@ -2,16 +2,42 @@ import math
 from collections import Counter
 from fractions import Fraction
 
-# Tokenization
 def tokenize(text):
+    """
+    Tokenize a given text by splitting on whitespace.
+    
+    Args:
+    text (str): The input text.
+
+    Returns:
+    list of str: The list of tokens.
+    """
     return text.split()
 
-# N-gram counting
 def ngram_counts(tokens, n):
+    """
+    Count the n-grams in a list of tokens.
+
+    Args:
+    tokens (list of str): The list of tokens.
+    n (int): The length of n-grams to consider.
+
+    Returns:
+    Counter: A Counter object containing n-gram counts.
+    """
     return Counter(tuple(tokens[i:i+n]) for i in range(len(tokens) - n + 1))
 
-# BLEU
 def clipped_ngram_count(candidate_ngrams, reference_ngrams):
+    """
+    Calculate the clipped n-gram count for the candidate and reference n-grams.
+
+    Args:
+    candidate_ngrams (Counter): The candidate n-gram counts.
+    reference_ngrams (Counter): The reference n-gram counts.
+
+    Returns:
+    dict: A dictionary containing clipped n-gram counts.
+    """
     clipped_counts = {
         ngram: min(candidate_ngrams[ngram], reference_ngrams[ngram])
         for ngram in candidate_ngrams
@@ -19,6 +45,17 @@ def clipped_ngram_count(candidate_ngrams, reference_ngrams):
     return clipped_counts
 
 def modified_precision(candidate, reference, n):
+    """
+    Calculate the modified precision for the given candidate and reference texts.
+
+    Args:
+    candidate (list of str): The list of candidate tokens.
+    reference (list of str): The list of reference tokens.
+    n (int): The length of n-grams to consider.
+
+    Returns:
+    float: The modified precision.
+    """
     candidate_ngrams = ngram_counts(candidate, n)
     reference_ngrams = ngram_counts(reference, n)
     clipped_counts = clipped_ngram_count(candidate_ngrams, reference_ngrams)
@@ -30,11 +67,31 @@ def modified_precision(candidate, reference, n):
     return Fraction(clipped_count_sum, total_count_sum)
 
 def closest_length_ratio(candidate_length, reference_lengths):
+    """
+    Calculate the closest length ratio between the candidate and reference texts.
+
+    Args:
+    candidate_length (int): The length of the candidate text.
+    reference_lengths (list of int): A list of lengths for each reference text.
+
+    Returns:
+    float: The closest length ratio.
+    """
     ref_diffs = [abs(candidate_length - ref_length) for ref_length in reference_lengths]
     closest_length = reference_lengths[ref_diffs.index(min(ref_diffs))]
     return candidate_length / closest_length
 
 def brevity_penalty(candidate_length, reference_lengths):
+    """
+    Calculate the brevity penalty for the given candidate and reference texts.
+
+    Args:
+    candidate_length (int): The length of the candidate text.
+    reference_lengths (list of int): A list of lengths for each reference text.
+
+    Returns:
+    float: The brevity penalty.
+    """
     c = candidate_length
     r = closest_length_ratio(candidate_length, reference_lengths)
     
@@ -44,45 +101,65 @@ def brevity_penalty(candidate_length, reference_lengths):
         return math.exp(1 - r)
 
 def bleu_score(candidate, references, max_n=4, weights=None):
+    """
+    Calculate BLEU score given a candidate translation and a list of reference translations.
+
+    Args:
+    candidate (str): The candidate translation string.
+    references (list of str): A list of reference translation strings.
+    max_n (int, optional): The maximum length of n-grams to consider(up to 4). Defaults to 4.
+    weights (list of float, optional): The weights for the modified precision scores of each n-gram length. Defaults to uniform weights.
+
+    Returns:
+    float: The BLEU score (0-1) for the candidate translation.
+    """
     if weights is None:
         weights = [1 / max_n] * max_n
 
     candidate_tokens = tokenize(candidate)
-    reference_tokens = [tokenize(ref) for ref in references]
+    reference_tokens_list = [tokenize(ref) for ref in references]
 
     candidate_length = len(candidate_tokens)
-    reference_lengths = [len(ref_tokens) for ref_tokens in reference_tokens]
+    reference_lengths = [len(ref_tokens) for ref_tokens in reference_tokens_list]
+
+    bp = brevity_penalty(candidate_length, reference_lengths)
 
     precisions = [
         modified_precision(candidate_tokens, ref_tokens, n)
         for n in range(1, max_n + 1)
-        for ref_tokens in reference_tokens
+        for ref_tokens in reference_tokens_list
     ]
 
-    weighted_log_precisions = [
-        weight * math.log(precision) if precision > 0 else 0
-        for weight, precision in zip(weights, precisions)
-    ]
+    weighted_precisions = [w * math.log(p) for w, p in zip(weights, precisions)]
+    bleu = bp * math.exp(sum(weighted_precisions))
 
-    score = brevity_penalty(candidate_length, reference_lengths) * math.exp(sum(weighted_log_precisions))
-    return score
-
-# ROUGE
-def rouge_n(candidate, reference, n):
-    candidate_tokens = tokenize(candidate)
-    reference_tokens = tokenize(reference)
-
-    candidate_ngrams = ngram_counts(candidate_tokens, n)
-    reference_ngrams = ngram_counts(reference_tokens, n)
-
-    overlapping_ngrams = sum(min(candidate_ngrams[ngram], reference_ngrams[ngram]) for ngram in candidate_ngrams)
-    total_reference_ngrams = sum(reference_ngrams.values())
-
-    if total_reference_ngrams == 0:
-        return 0
-
-    return overlapping_ngrams / total_reference_ngrams
+    return bleu
 
 def rouge_score(candidate, references, n=2):
-    scores = [rouge_n(candidate, reference, n) for reference in references]
-    return max(scores)
+    """
+    Calculate ROUGE-N score given a candidate summary and a list of reference summaries.
+    
+    Args:
+    candidate (str): The candidate summary string.
+    references (list of str): A list of reference summary strings.
+    n (int, optional): The length of n-grams to consider. Defaults to 2 (bigrams).
+
+    Returns:
+    float: The ROUGE-N score (0-1) for the candidate summary.
+    """
+    candidate_tokens = tokenize(candidate)
+    reference_tokens_list = [tokenize(ref) for ref in references]
+
+    candidate_ngrams = ngram_counts(candidate_tokens, n)
+    reference_ngrams_list = [ngram_counts(ref_tokens, n) for ref_tokens in reference_tokens_list]
+
+    max_clipped_counts = Counter()
+    for ref_ngrams in reference_ngrams_list:
+        clipped_counts = clipped_ngram_count(candidate_ngrams, ref_ngrams)
+        max_clipped_counts = max(max_clipped_counts, clipped_counts, key=lambda c: sum(c.values()))
+
+    rouge_numerator = sum(max_clipped_counts.values())
+    rouge_denominator = sum(candidate_ngrams.values())
+
+    return rouge_numerator / rouge_denominator
+
